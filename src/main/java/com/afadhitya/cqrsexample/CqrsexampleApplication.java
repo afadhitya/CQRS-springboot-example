949292fbf67802569c0567d65f271c7b04fa674b
Achmad Fadhitya <afadhitya@gmail.com>
2024-03-11 11:19:51 +0700

    Initial commit

---
+++ src/main/java/com/afadhitya/cqrsexample/CqrsexampleApplication.java
@@ -0,0 +1,13 @@
+package com.afadhitya.cqrsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afadhitya/cqrsexample/CqrsexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afadhitya.cqrsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
